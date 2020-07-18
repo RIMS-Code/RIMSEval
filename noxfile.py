8182f0f50ceb3be12542ffabd9a19cfb5e0de9be
Reto Trappitsch <reto@galactic-forensics.space>
2020-07-18 16:27:38 -0700

    Start of project check in
    Create testing environment, requirements files, package files, inits,...
    Not sure yet if GUI will live in the same repo.

---
+++ noxfile.py
@@ -0,0 +1,67 @@
+"""Configuration file for testing the package with ``nox``."""
+
+import nox
+
+
+nox.options.sessions = "lint", "safety", "tests"
+
+package = "rimseval"
+locations = "rimseval", "noxfile.py"
+python_suite = ["3.8", "3.7", "3.6"]
+
+
+@nox.session(python="3.8")
+def black(session):
+    """Autoformat all python files with black."""
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+# @nox.session(python="3.8")
+# def build(session):
+#     """Pack rimstiming for release on PyPi."""
+#     session.install("flit")
+#     session.run("flit", "build")
+
+
+# @nox.session(python="3.8")
+# def docs(session):
+#     """Build the documentation."""
+#     session.install("sphinx", "sphinx_rtd_theme", "-r", "requirements.txt", "pytest")
+#     session.chdir("docs")
+#     session.run(
+#         "sphinx-build", "-b", "html", ".", "_build/html/"
+#     )  # as for readthedocs.io
+
+
+@nox.session(python=python_suite)
+def lint(session):
+    """Lint project using ``flake8``."""
+    args = session.posargs or locations
+    session.install("-r", "dev-requirements.txt")
+    session.run("flake8", *args)
+
+
+@nox.session(python=python_suite)
+def tests(session):
+    """Test the project using ``pytest``."""
+    session.install("-r", "requirements.txt", "-r", "dev-requirements.txt")
+    session.run("pytest")
+
+
+@nox.session(python="3.8")
+def safety(session):
+    """Safety check for all dependencies."""
+    session.install("safety", "-r", "requirements.txt", "-r", "dev-requirements.txt")
+    session.run(
+        "safety", "check", "--full-report",
+    )
+
+
+@nox.session(python=python_suite)
+def xdoctest(session):
+    """Test docstring examples with xdoctest."""
+    args = session.posargs or ["all"]
+    session.install("xdoctest[all]", "-r", "requirements.txt")
+    session.run("python", "-m", "xdoctest", package, *args)
---
+++ rimseval/__init__.py
@@ -0,0 +1,17 @@
+"""Initialization of the rimseval package."""
+
+# Package information
+__version__ = "0.0.0"
+
+__title__ = "rimseval"
+__description__ = (
+    "Evaluate resonance ionization mass spectrometry measurements, correct, and filter. Contains a reader for"
+    "crd files (Chicago Raw Data) but can also convert fastcomtec lst files to crd."
+)
+
+# Todo
+__uri__ = "Todo Docs Link"
+__author__ = "Reto Trappitsch"
+
+__license__ = "GPLv2"
+__copyright__ = "Copyright (c) 2020, Reto Trappitsch"
