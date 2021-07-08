53c12dad4b699691a9805dff12d53a3d597b7827
Reto Trappitsch <reto@brandeis.edu>
2021-07-08 18:17:10 -0400

    Edit __init__.py - Typo corrections

--- rimseval/__init__.py
+++ rimseval/__init__.py
@@ -8,13 +8,14 @@ __version__ = "0.0.0"
 
 __title__ = "rimseval"
 __description__ = (
-    "Evaluate resonance ionization mass spectrometry measurements, correct, and filter. Contains a reader for"
-    "crd files (Chicago Raw Data) but can also convert fastcomtec lst files to crd."
+    "Evaluate resonance ionization mass spectrometry measurements, correct, and "
+    "filter. Contains a reader for crd files (Chicago Raw Data) but can also convert "
+    "FastComTec lst files to crd."
 )
 
 # Todo
 __uri__ = "Todo Docs Link"
 __author__ = "Reto Trappitsch"
 
-__license__ = "GPLv2"
+__license__ = "MIT"
 __copyright__ = "Copyright (c) 2020, Reto Trappitsch"
