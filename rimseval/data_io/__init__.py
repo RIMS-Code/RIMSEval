30474c5fe30c1691b3be8e6b574114d5245c0bbb
Reto Trappitsch <reto@galactic-forensics.space>
2022-02-14 21:53:10 -0500

    Add export capabilities for mass spec and tof spec

--- rimseval/data_io/__init__.py
+++ rimseval/data_io/__init__.py
@@ -4,9 +4,10 @@ This routine turns list files into crd files. The crd file format is as specifie
 the `docs` folder and adheres currently to v1.0 of the format.
 """
 
+from . import export
 from . import crd_utils
 from . import lst_utils
 from .crd_reader import CRDReader
 from .lst_to_crd import LST2CRD
 
-__all__ = ["crd_utils", "lst_utils", "CRDReader", "LST2CRD"]
+__all__ = ["export", "crd_utils", "lst_utils", "CRDReader", "LST2CRD"]
