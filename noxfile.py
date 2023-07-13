03dbf956517c04d68fffa95ee2f433d8dbd46099
Reto Trappitsch <reto@galactic-forensics.space>
2023-07-13 12:27:54 +0200

    Upgrade numba to 0.57.1

--- noxfile.py
+++ noxfile.py
@@ -2,13 +2,12 @@
 
 import nox
 
-# fixme: add safety to nox
 nox.options.sessions = ["lint", "tests"]
 
 package = "rimseval"
 locations = "rimseval", "noxfile.py"
-python_default = "3.10"
-python_suite = ["3.10", "3.9", "3.8"]
+python_default = "3.11"
+python_suite = ["3.11", "3.10", "3.9", "3.8"]
 
 
 @nox.session(python=python_default)
@@ -34,14 +33,3 @@ def tests(session):
     """Test the project using ``pytest``."""
     session.install(".[test]")
     session.run("pytest")
-
-
-@nox.session(python=python_default)
-def safety(session):
-    """Safety check for all dependencies."""
-    session.install("safety", ".")
-    session.run(
-        "safety",
-        "check",
-        "--full-report",
-    )
